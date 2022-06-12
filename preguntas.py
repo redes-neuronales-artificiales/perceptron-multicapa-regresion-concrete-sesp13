2d23e70ef6cefaf7eae1ee4e61415dce82f27bf8
sesp13 <sespinosarte99@gmail.com>
2022-06-11 22:46:26 -0500

    test question 1

--- preguntas.py
+++ preguntas.py
@@ -16,19 +16,19 @@ def pregunta_01():
     Carga y separación de los datos en `X` `y`
     """
     # Lea el archivo `concrete.csv` y asignelo al DataFrame `df`
-    df = ____  
+    df = pd.read_csv("concrete.csv")
 
     # Asigne la columna `strength` a la variable `y`.
-    ____ = ____  
+    y = df["strength"]  
 
     # Asigne una copia del dataframe `df` a la variable `X`.
-    ____ = ____.____(____)  
+    X = df.copy()
 
     # Remueva la columna `strength` del DataFrame `X`.
-    ____.____(____)  
+    X.drop('strength', axis=1, inplace=True)
 
     # Retorne `X` y `y`
-    return x, y
+    return X, y
 
 
 def pregunta_02():
